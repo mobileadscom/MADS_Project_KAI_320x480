122d6c52bd91cfa13b3831957e200f066260fc9f
Jeff Paltera <jeffpaltera@outlook.com>
2017-10-26 22:38:12 +0800

    first commit

---
+++ src/js/handlers.js
@@ -0,0 +1,2 @@
+/* eslint-disable */
+gwd.auto_Gwd_taparea_1Action = function (event) { gwd.actions.gwdGenericad.exit('gwd-ad', 'https://itunes.apple.com/id/app/kai-access/id901804734?mt=8', true); }; gwd.auto_Gwd_taparea_2Action = function (event) { gwd.actions.gwdGenericad.exit('gwd-ad', 'https://play.google.com/store/apps/details?id=com.kai.kaiticketing&hl=en', true); };
